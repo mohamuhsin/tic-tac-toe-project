168dd1128f9bcbb68a3bb66c8a62654f67b67950
Abraham Kiplagat <kurerelagat01@.com>
2023-11-29 11:02:42 +0300

    fixed

--- Abraham/src/App.jsx
+++ Abraham/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players">
+          {/* props */}
           <Player initialName="Player 1" symbol="X" />
           <Player initialName="Player 2" symbol="0" />
         </ol>
