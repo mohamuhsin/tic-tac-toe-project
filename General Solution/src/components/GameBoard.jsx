a40d8f2e5ffd69986dcf46d3d6feab1cdd1b9d41
mohamuhsin <muhsinohoho@outlook.com>
2023-11-30 23:27:01 +0300

    updated the states of the squares when the square is clicked on the board

--- General Solution/src/components/GameBoard.jsx
+++ General Solution/src/components/GameBoard.jsx
@@ -1,17 +1,35 @@
+import { useState } from "react";
+
 const initialBoardGame = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialBoardGame);
+
+  function handleSelectedSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  }
   return (
     <ol id="game-board">
-      {initialBoardGame.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button
+                  onClick={() => handleSelectedSquare(rowIndex, colIndex)}
+                >
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
