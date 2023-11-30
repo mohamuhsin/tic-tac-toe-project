6aefd7c213a6a402e5285c6acc0008d9131a2d61
mohamuhsin <muhsinohoho@outlook.com>
2023-11-30 23:08:05 +0300

    created gameBoard component and populated with 3 by 3 squares

---
+++ General Solution/src/components/GameBoard.jsx
@@ -0,0 +1,22 @@
+const initialBoardGame = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+export default function GameBoard() {
+  return (
+    <ol id="game-board">
+      {initialBoardGame.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
